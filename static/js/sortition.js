5664ed986cb6f9ca33867ac093922b2858011d40
Rafael Cavasani <rafael.cavasani@gmail.com>
2020-02-08 11:15:11 -0300

    Add Javascript puro

--- static/js/sortition.js
+++ static/js/sortition.js
@@ -1,34 +1,77 @@
-const btnSortition = document.getElementById('btnSortition');
 const onClickBtnSortition = () => {
     let list = [];
-    const checkboxs = document.getElementsByTagName('input[type=checkbox]');
-    checkboxs.forEach(element => {
-        
-    });
-    $('input[type=checkbox]').each(() => {
-        if ($(this).is(':checked')) {
-            const name = $(this).parent().parent().find('td').eq(1).text();
-            const position = $(this).parent().parent().find('td').eq(2).text();
-            const importance = $(this).parent().parent().find('td').eq(3).text();
+    positions = {
+        "Goleiro": 0,
+        "Zagueiro": 1,
+        "Lateral": 2,
+        "Meio Campo": 3,
+        "Atacante": 4
+    };
+    const checkboxs = document.getElementsByClassName('checkbox');
+    for (let i = 0; i < checkboxs.length; i++) {
+        if (checkboxs[i].checked) {
+            const item = checkboxs[i].parentElement.parentElement;
+            const name = item.children[1].innerText;
+            const position = item.children[2].innerText;
+            const importance = item.children[3].innerText;
             const player = {
                 name: name,
-                position: position,
+                position: positions[position],
                 importance: importance
             }
             list.push(player);
         }
-    });
-};
-btnSortition.addEventListener('click', onClickBtnSortition);
+    }
+    checkPlayersToSort(list);
+}
 
 
-const checkboxs = document.getElementsByTagName('input[type=checkbox]');
-checkboxs.addEventListener('change', () => {
-    let check = 0;
-    checkboxs.forEach(element => {
-        if (element.checked) check++;
-    });
-
+let check = 0;
+const checkboxChanged = (element) => {
+    if (element.target.checked) check++;
+    else check--;
     const countSelected = document.getElementById('countSelected');
-    countSelected.innerHTML = countSelected;
-});
+    countSelected.innerHTML = check;
+}
+
+const checkAll = () => {
+    const checkbox = document.getElementById('checkall');
+    const checkboxs = document.getElementsByClassName('checkbox');
+    for (let i = 0; i < checkboxs.length; i++) {
+        checkboxs[i].checked = checkbox.checked;
+    }
+}
+
+function checkPlayersToSort(list) {
+    list.sort((a, b) => {
+        if (a.position > b.position) return 1;
+        if (a.position < b.position) return -1;
+        return 0;
+    });
+    if (list.length < 6) {
+        alert('Você deve selecionar ao menos 6 jogadores para realizar o sorteio.');
+        return false;
+    }
+    goalkeepers = list.filter((a) => a.position == 0);
+    if (goalkeepers.length != 2) {
+        alert('Você deve selecionar 2 goleiros para realizar o sorteio.');
+        return false;
+    }
+
+    console.log(goalkeepers);
+}
+
+(function() {
+    
+    const btnSortition = document.getElementById('btnSortition');
+    btnSortition.addEventListener('click', onClickBtnSortition);
+
+    const checkboxs = document.getElementsByClassName('checkbox');
+    for (let i = 0; i < checkboxs.length; i++) {
+        checkboxs[i].addEventListener('change', checkboxChanged);
+    }
+
+    const checkall = document.getElementById('checkall');
+    checkall.addEventListener('change', checkAll);
+
+})();
